5154443deb2bae54a09bda6000016ab18fbe26c5
Josh Quintana <josh_d_quintana@homedepot.com>
2016-01-22 21:07:32 -0500

    CLI driven

--- index.js
+++ index.js
@@ -8,33 +8,28 @@ const openRepo = Q.denodeify( ( ...args ) => new git.Repo( ...args ) );
 const rnd = ( min = 0, max = 1 ) => min + Math.round( Math.random() * ( max - min ) );
 
 const today = new Date();
-const yearAgo = ( () => {
-	let date = new Date();
-	date.setDate( date.getDate() - 365 );
-	return date;
-} )();
+const yearAgo = new Date( Date.now() - 365 * 24 * 60 * 60 * 1000 );
 
 function rndmDate () {
-	return new Date( rnd( yearAgo.getTime(), today.getTime() ) );
+  return new Date( rnd( yearAgo.getTime(), today.getTime() ) );
 }
 
 function makeCommit ( repo, date ) {
-	const isodate = date.toISOString();
-	process.env.GIT_AUTHOR_DATE = process.env.GIT_COMMITTER_DATE = isodate;
-	return Q.nfcall( fs.appendFile, fil, `// ${ isodate }\n`, {} )
-		.then( () => Q.nfcall( ::repo.commit_all, isodate ) );
+  const isodate = date.toISOString();
+  process.env.GIT_AUTHOR_DATE = process.env.GIT_COMMITTER_DATE = isodate;
+  return Q.nfcall( fs.appendFile, fil, `// ${ isodate }\n`, {} )
+    .then( () => Q.nfcall( ::repo.commit_all, isodate ) );
 }
 
 function makeCommits ( repo, commits = 100 ) {
-	return Q.async( function* docommits () {
-		while ( commits-- ) {
-			let date = rndmDate();
-			yield makeCommit( repo, date );
-			console.log( commits );
-		}
-	} )();
+  return Q.async( function* docommits () {
+    while ( commits-- ) {
+      yield makeCommit( repo, rndmDate() );
+      console.log( commits );
+    }
+  } )();
 }
 
 openRepo( __dirname, {} )
-	.then( repo => makeCommits( repo, 10 ) )
-	;
+  .then( repo => makeCommits( repo, process.argv[ 2 ] ) )
+  ;
